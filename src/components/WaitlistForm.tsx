ba1eadda6bdd319b0ab949912ae4fce55c60edc7
suhas-sensei <suhas.ghosal2002@gmail.com>
2026-03-10 19:01:31 +0530

    redirect

--- src/components/WaitlistForm.tsx
+++ src/components/WaitlistForm.tsx
@@ -4,7 +4,7 @@ export default function WaitlistForm() {
     return (
         <div className="flex flex-col items-center w-full mt-0 gap-6">
             <a
-                href="https://app.rezlabs.org/"
+                href="https://app.rezlabs.org/?connect=true"
                 className="btn launch-app-btn !rounded-xl cursor-pointer whitespace-nowrap !min-h-[50px] !h-[50px] !py-0 !px-[2.25rem] text-[1.1rem] max-[768px]:text-[1.3rem] text-center"
             >
                 LAUNCH APP
